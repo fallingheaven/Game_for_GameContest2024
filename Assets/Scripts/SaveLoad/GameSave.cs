3d1c4745e97e8ba2f5667e3f64460e6dcb3dfde4
fallingheaven <296142289@qq.com>
2024-04-04 22:43:27 +0800

    SaveLoad框架

---
+++ Assets/Scripts/SaveLoad/GameSave.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using Utility.CustomClass;
+
+namespace SaveLoad
+{
+    public class GameSaveArray
+    {
+        public ObservedList<GameSave> saves = new ObservedList<GameSave>();
+    }
+    
+    public class GameSave
+    {
+        public int levelIndex;
+        public float playTime;
+    
+        public GameSave()
+        {
+            levelIndex = 0;
+            playTime = 0f;
+        }
+        
+        public void ResetSave()
+        {
+            levelIndex = 0;
+            playTime = 0f;
+        }
+    }
+}
+
---
+++ Assets/Scripts/Utility/CustomClass/ObservedList.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using UnityEngine.Events;
+
+namespace Utility.CustomClass
+{
+    public class ObservedList<T> : List<T>
+    {
+        public UnityAction onChanged;
+        
+        public new void Add(T item)
+        {
+            base.Add(item);
+            OnChanged();
+        }
+
+        public new void Remove(T item)
+        {
+            base.Remove(item);
+            OnChanged();
+        }
+
+        public new void RemoveAt(int index)
+        {
+            base.RemoveAt(index);
+            OnChanged();
+        }
+
+        private void OnChanged()
+        {
+            onChanged?.Invoke();
+        }
+    }
+}
